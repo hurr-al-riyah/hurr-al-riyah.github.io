af9e7c582de64dc87804349b177b80b7259551d6
hurr-al-riyah <hattarck+hurr@gmail.com>
2023-11-13 23:50:28 +0900

    version 1.6.2 update
    - add csv data of 1111 race
    - add dataPathGenerator

---
+++ lib/dataPathGenerator/dataPathGenerator.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+def list_csv_files(directory):
+    return [file for file in os.listdir(directory) if file.endswith('.csv')]
+
+def generate_json_structure(base_directory):
+    json_structure = {}
+
+    for subdir in os.listdir(base_directory):
+        dir_path = os.path.join(base_directory, subdir)
+        
+        if os.path.isdir(dir_path):
+            json_structure[subdir] = list_csv_files(dir_path)
+    
+    return json_structure
+
+base_directory = '../../data/클래식_1111'
+
+json_data = generate_json_structure(base_directory)
+
+json_str = json.dumps(json_data, indent=4, ensure_ascii=False)
+print(json_str)
