b8f4c92bc0e0029a2d95493045df3c46c76da39a
hurr-al-riyah <hattarck+hurr@gmail.com>
2023-12-10 01:36:35 +0900

    v1.8.2 update
    - add 12/9 race data
    - edit footer, mobile plan

--- lib/csv2json/csv2json.py
+++ lib/csv2json/csv2json.py
@@ -1,8 +1,8 @@
 import csv
 import json
 
-csv_file_path = 'data/stat_1111.csv'
-json_file_path = 'data/umaStats2.json'
+csv_file_path = 'hurr-al-riyah.github.io/lib/csv2json/data/stat_1209.csv'
+json_file_path = 'hurr-al-riyah.github.io/lib/csv2json/data/umaStats2.json'
 
 new_data = {}
 
--- lib/dataPathGenerator/dataPathGenerator.py
+++ lib/dataPathGenerator/dataPathGenerator.py
@@ -15,7 +15,7 @@ def generate_json_structure(base_directory):
     
     return json_structure
 
-base_directory = '../../data/클래식_1111'
+base_directory = 'hurr-al-riyah.github.io/data/클래식_1111'
 
 json_data = generate_json_structure(base_directory)
 
