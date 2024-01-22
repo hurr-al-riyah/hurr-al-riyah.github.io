650d00e00637e0a12e2ef6f90ed8796bc93a06ee
hurr-al-riyah <hattarck+hurr@gmail.com>
2024-01-22 20:21:43 +0900

    v1.12.2 update
    - jan. 13 race image data update

--- lib/dataPathGenerator/dataPathGenerator.py
+++ lib/dataPathGenerator/dataPathGenerator.py
@@ -15,7 +15,9 @@ def generate_json_structure(base_directory):
     
     return json_structure
 
-base_directory = 'hurr-al-riyah.github.io/data/시니어_0106'
+print(os.getcwd())
+
+base_directory = '../../data/시니어_0113'
 
 json_data = generate_json_structure(base_directory)
 
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version="1.12.1"
+const version="1.12.2"
 
 let globalData = null;  // graph data 
 let raceCategory = "";   // ex) 주니어_1007
